f05c93127edf79296a5f97caed9437a092ebb9bb
Shiimymy <>
2023-08-15 15:57:02 +0000

    Add get_new_survey_data function to get data from user

--- run.py
+++ run.py
@@ -15,3 +15,27 @@ SHEET = GSPREAD_CLIENT.open('survey_data')
 survey = SHEET.worksheet('survey')
 data = survey.get_all_values()
 print(data)
+
+
+def get_new_survey_data():
+    """
+    Get a new survey data with 3 information from the user.
+    The first should be a employee name
+    The second a number between 0 and 10
+    The last a 3 options string.
+    """
+    new_survey = []
+
+    print('Please enter the results of the new survey\n')
+    new_name = input('Enter the employee name: \n')
+    new_survey.append(new_name)
+    new_nps = input('Enter a number between 0 and 10: \n')
+    new_survey.append(new_nps)
+    new_resolve = input("Enter 'yes', 'no' or 'I don't know': \n")
+    new_survey.append(new_resolve)
+
+    print(f"The data from the surrvey is:{new_survey}")
+
+
+get_new_survey_data()
+
