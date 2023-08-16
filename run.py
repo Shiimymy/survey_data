ece3dec6038d77a6245f00f4f1eeaac8525d6873
Shiimymy <>
2023-08-16 08:18:19 +0000

    Add validate_data function to check user input

--- run.py
+++ run.py
@@ -27,15 +27,44 @@ def get_new_survey_data():
     new_survey = []
 
     print('Please enter the results of the new survey\n')
-    new_name = input('Enter the employee name: \n')
+    new_name = input('Enter the employee name:')
     new_survey.append(new_name)
-    new_nps = input('Enter a number between 0 and 10: \n')
-    new_survey.append(new_nps)
-    new_resolve = input("Enter 'yes', 'no' or 'I don't know': \n")
+    new_nps = input('Enter a number between 0 and 10:')
+    new_survey.append(int(new_nps))
+    new_resolve = input("Enter 'yes', 'no' or 'I don't know':\n")
     new_survey.append(new_resolve)
+    
+    validate_data(new_survey)
 
-    print(f"The data from the surrvey is:{new_survey}")
 
+def validate_data(values):
+    """
+    Check if the 3 datas provided by the user are valid.
+    """
+    
+    if (values[0]).isalpha():
+        print("Valid name")
+
+        if 0 <= values[1] <= 10:
+            print("Valid NPS")
+            
+            if values[2] == "yes":
+                return values
+            elif values[2] == "no":
+                return values
+            elif values[2] == "I don't know":
+                return values
+            else:
+                print("The input can only be 'yes', 'no' or 'I don't know'\n")
+                get_new_survey_data()
+
+        else:
+            print("The NPS should be a number between 0 and 10\n")
+            get_new_survey_data()
+
+    else:
+        print("Please enter a name with only letters from the alphabet\n")
+        get_new_survey_data()
+        
 
 get_new_survey_data()
-
