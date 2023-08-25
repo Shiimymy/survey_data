f2a3dfdef69b07a44f801292637083640d619c6e
Shiimymy <>
2023-08-25 20:06:21 +0000

    Add functions to get leader action in score worksheet

--- run.py
+++ run.py
@@ -23,11 +23,12 @@ def get_new_survey_data():
     new_survey = []
 
     print('Please enter the results of the new survey\n')
-    new_name = input('Enter the employee name:')
+    new_name = input('Enter the employee name:\n')
     new_survey.append(new_name.capitalize())
     # bug to fix : add ValueError exception
-    new_nps = input('Enter a number between 0 and 10:')
+    new_nps = input('Enter a number between 0 and 10:\n')
     new_survey.append(int(new_nps))
+    print("How the customers reponded to : Did you issue was resolved?")
     new_resolve = input("Enter 'yes', 'no' or 'i don't know':\n")
     new_survey.append(new_resolve.lower())
     
@@ -43,11 +44,13 @@ def validate_data(values):
     # bug to fix : doesn't get new value to worksheet
     if (values[0]).isalpha():
         print("Valid name")
+        # new function 
 
         if 0 <= values[1] <= 10:
             print("Valid NPS")
-            
+            # new function
             if values[2] == "yes":
+                # new function after user input
                 return values
             elif values[2] == "no":
                 return values
@@ -109,13 +112,51 @@ def calculate_resolution_percentage(employee_list):
     return round(percentage_yes)
 
 
-def update_score_worksheet(nps, resolution):
+def identify_leader_meeting(nps, resolution):
+    """
+    Identify if the Team Leader would need to organise a meeting
+    to improve Team performances.
+    """
+    meeting_action = 5 <= nps <= 7 or 55 <= resolution <= 75
+
+    if meeting_action:
+        meeting = "Meeting"
+        return meeting
+    else:
+        no_action = ""
+        return no_action
+
+
+def identify_leader_training(nps, resolution):
+    """
+    Identify if the Team Leader would need to organise a training
+    to improve Team performances.
+    """
+    training_action = nps < 5 and resolution < 55
+
+    if training_action:
+        training = "Training"
+        return training
+    else:
+        no_action = ""
+        return no_action
+
+
+def get_leader_action(meeting, training):
+    if not (meeting or training):
+        no_action = "None"
+        return no_action
+    else: 
+        return f"{meeting}{training}"
+
+
+def update_score_worksheet(nps, resolution, action):
     """
     Update the score worksheet with actions to plan for the user
     wich are linked with the average NPS score of the Team
     and the issue resolution percentage.
     """
-    new_score = ["action", nps, f"{resolution}%"]
+    new_score = ["date", nps, f"{resolution}%", action]
     score_worksheet = SHEET.worksheet("score")
     score_worksheet.append_row(new_score)
     print("The score worksheet is updated: please check your Team score.")
@@ -130,7 +171,10 @@ def main():
     employee_list = get_employees()
     average_nps = get_average_nps(employee_list)
     resolution_percentage = calculate_resolution_percentage(employee_list)
-    update_score_worksheet(str(average_nps), str(resolution_percentage))
+    meeting = identify_leader_meeting(average_nps, resolution_percentage)
+    training = identify_leader_training(average_nps, resolution_percentage)
+    action = get_leader_action(meeting, training)
+    update_score_worksheet(str(average_nps), str(resolution_percentage), str(action))
 
 
 main()
