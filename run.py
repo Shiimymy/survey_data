0d22e477b4f26afb10fd3248d7e6885e04d8aa22
Shiimymy <>
2023-08-27 08:41:30 +0000

    Update get_new_survey_data func to create 3 new functions for clarity

--- run.py
+++ run.py
@@ -14,7 +14,25 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('survey_data')
 
 
-def get_new_survey_data():
+def get_new_name():
+    print('Please enter the results of the new survey\n')
+    new_name = input('Enter the employee name:\n')
+    validate_name(new_name)
+    return new_name
+
+
+def get_new_nps():
+    new_nps = input('Enter a number between 0 and 10:\n')
+    return new_nps
+
+
+def get_new_resolution():
+    print("How the customers reponded to : Did you issue was resolved?")
+    new_resolve = input("Enter 'yes', 'no' or 'i don't know':\n")
+    return new_resolve
+
+
+def get_new_survey_data(new_name, new_nps, new_resolve):
     """
     Get a new survey data with 3 information from the user.
     The first should be a employee name
@@ -23,19 +41,24 @@ def get_new_survey_data():
     """
     new_survey = []
 
-    print('Please enter the results of the new survey\n')
-    new_name = input('Enter the employee name:\n')
     new_survey.append(new_name.capitalize())
-    # bug to fix : add ValueError exception
-    new_nps = input('Enter a number between 0 and 10:\n')
     new_survey.append(int(new_nps))
-    print("How the customers reponded to : Did you issue was resolved?")
-    new_resolve = input("Enter 'yes', 'no' or 'i don't know':\n")
     new_survey.append(new_resolve.lower())
-    
+            
     validate_data(new_survey)
 
     return new_survey
+
+
+def validate_name(new_name):
+    """
+    Validate name given by user
+    """
+    if (new_name).isalpha():
+        print(f"Valid Name: {new_name}")
+        return new_name
+    else:
+        print(f"wrong : {new_name}")
 
 
 def validate_data(values):
@@ -43,31 +66,20 @@ def validate_data(values):
     Check if the 3 datas provided by the user are valid.
     """
     # bug to fix : doesn't get new value to worksheet
-    if (values[0]).isalpha():
-        print("Valid name")
-        # new function 
-
-        if 0 <= values[1] <= 10:
-            print("Valid NPS")
-            # new function
-            if values[2] == "yes":
-                # new function after user input
-                return values
-            elif values[2] == "no":
-                return values
-            elif values[2] == "i don't know":
-                return values
-            else:
-                print("The input can only be 'yes', 'no' or 'I don't know'\n")
-                get_new_survey_data()
-
+    if 0 <= values[1] <= 10:
+        print("Valid NPS")
+        if values[2] == "yes":
+            # new function after user input
+            return values
+        elif values[2] == "no":
+            return values
+        elif values[2] == "i don't know":
+            return values
         else:
-            print("The NPS should be a number between 0 and 10\n")
-            get_new_survey_data()
-
+            print("The input can only be 'yes', 'no' or 'I don't know'\n")
+            
     else:
-        print("Please enter a name with only letters from the alphabet\n")
-        get_new_survey_data()
+        print("The NPS should be a number between 0 and 10\n")
         
 
 def update_survey_worksheet(data):
@@ -162,13 +174,14 @@ def get_date():
     date = datetime.now().date()
     return date
 
+
 def update_score_worksheet(date, nps, resolution, action):
     """
     Update the score worksheet with actions to plan for the user
     wich are linked with the average NPS score of the Team
     and the issue resolution percentage.
     """
-    new_score = [str(date), nps, f"{resolution}%", action]
+    new_score = [str(date), str(nps), f"{resolution}%", str(action)]
     score_worksheet = SHEET.worksheet("score")
     score_worksheet.append_row(new_score)
     print("The score worksheet is updated: please check your Team score.")
@@ -178,16 +191,19 @@ def main():
     """
     Run all program functions.
     """
-    new_data = get_new_survey_data()
+    new_name = get_new_name()
+    new_nps = get_new_nps()
+    new_resolve = get_new_resolution()
+    new_data = get_new_survey_data(new_name, new_nps, new_resolve)
     update_survey_worksheet(new_data)
     employee_list = get_employees()
     average_nps = get_average_nps(employee_list)
-    resolution_percentage = calculate_resolution_percentage(employee_list)
-    meeting = identify_leader_meeting(average_nps, resolution_percentage)
-    training = identify_leader_training(average_nps, resolution_percentage)
+    reso_percentage = calculate_resolution_percentage(employee_list)
+    meeting = identify_leader_meeting(average_nps, reso_percentage)
+    training = identify_leader_training(average_nps, reso_percentage)
     action = get_leader_action(meeting, training)
     date = get_date()
-    update_score_worksheet(date, str(average_nps), str(resolution_percentage), str(action))
+    update_score_worksheet(date, average_nps, str(reso_percentage), action)
 
 
 main()
